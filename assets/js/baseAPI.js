434774c6c98b28378f6e17c0fffc95f4c59ca21b
mhz <954213532@qq.com>
2021-01-19 21:10:39 +0800

    首页完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,17 @@ $.ajaxPrefilter(function (options) {
     // console.log(options);
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
     // console.log(options.url);
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization:localStorage.getItem('token') || ''
+        }
+    }
+    options.complete = function (res) {
+        console.log('执行了complete回调');
+        console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            localStorage.removeItem("token")
+            location.href = '/login.html'
+        }
+    }
 })
