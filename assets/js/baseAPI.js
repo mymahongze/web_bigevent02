c0eeaf4331ac7326ebf82ff6bc29143d98867184
mhz <954213532@qq.com>
2021-01-19 22:41:20 +0800

    个人中心页面完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -11,8 +11,8 @@ $.ajaxPrefilter(function (options) {
         }
     }
     options.complete = function (res) {
-        console.log('执行了complete回调');
-        console.log(res);
+        // console.log('执行了complete回调');
+        // console.log(res);
         if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
             localStorage.removeItem("token")
             location.href = '/login.html'
